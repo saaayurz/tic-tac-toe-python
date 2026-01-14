620d07e9284d144f598fa0e6626d360d95408f2b
saaayurz <nexus.saaayurkhaparde@gmail.com>
2026-01-14 16:51:38 +0530

    Initial project setup and empty board display

---
+++ game.py
@@ -0,0 +1,9 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+print("Tic Tac Toe Board:")
+print(board[0] + " | " + board[1] + " | " + board[2])
+print("--+---+--")
+print(board[3] + " | " + board[4] + " | " + board[5])
+print("--+---+--")
+print(board[6] + " | " + board[7] + " | " + board[8])
+
