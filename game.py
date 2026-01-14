5b5cd10e19a4750548ea30a1f30d9ae58db885ff
saaayurz <nexus.saaayurkhaparde@gmail.com>
2026-01-14 20:15:14 +0530

    Add two-player X and O moves

--- game.py
+++ game.py
@@ -1,27 +1,25 @@
 # Create empty board
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-# Display the board
-print("Tic Tac Toe Board:")
-print(board[0] + " | " + board[1] + " | " + board[2])
-print("--+---+--")
-print(board[3] + " | " + board[4] + " | " + board[5])
-print("--+---+--")
-print(board[6] + " | " + board[7] + " | " + board[8])
+# Function to display board
+def show_board():
+    print()
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("--+---+--")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("--+---+--")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print()
 
-# Ask player for a move
-position = input("Enter a position (1-9): ")
+# Show empty board
+show_board()
 
-# Convert input to number and adjust index
-index = int(position) - 1
+# Player X move
+x_pos = int(input("Player X, enter position (1-9): ")) - 1
+board[x_pos] = "X"
+show_board()
 
-# Place X on the board
-board[index] = "X"
-
-# Show updated board
-print("\nUpdated Board:")
-print(board[0] + " | " + board[1] + " | " + board[2])
-print("--+---+--")
-print(board[3] + " | " + board[4] + " | " + board[5])
-print("--+---+--")
-print(board[6] + " | " + board[7] + " | " + board[8])
+# Player O move
+o_pos = int(input("Player O, enter position (1-9): ")) - 1
+board[o_pos] = "O"
+show_board()
