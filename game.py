195bd410a89e28f733e6654b578f11fbc10f1a3f
saaayurz <nexus.saaayurkhaparde@gmail.com>
2026-01-14 20:12:03 +0530

    Allow player to place X on the board

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
+# Create empty board
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+# Display the board
 print("Tic Tac Toe Board:")
 print(board[0] + " | " + board[1] + " | " + board[2])
 print("--+---+--")
@@ -7,3 +9,19 @@ print(board[3] + " | " + board[4] + " | " + board[5])
 print("--+---+--")
 print(board[6] + " | " + board[7] + " | " + board[8])
 
+# Ask player for a move
+position = input("Enter a position (1-9): ")
+
+# Convert input to number and adjust index
+index = int(position) - 1
+
+# Place X on the board
+board[index] = "X"
+
+# Show updated board
+print("\nUpdated Board:")
+print(board[0] + " | " + board[1] + " | " + board[2])
+print("--+---+--")
+print(board[3] + " | " + board[4] + " | " + board[5])
+print("--+---+--")
+print(board[6] + " | " + board[7] + " | " + board[8])
